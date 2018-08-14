469ba84e3ce33c86c4a676de375a9586fc96cf22
VolodymyrMarchenko <nokialeader@gmail.com>
2018-08-14 16:49:52 +0300

    remove fancybox

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,9 @@
-import '@fancyapps/fancybox';
-
-$('.fancybox').fancybox({
-  helpers : {
-    overlay : {
-      css : {
-        'background' : 'rgba(58, 42, 45, 0.95)'
-      }
-    }
-  }
-});
-
-
-
-
-
-
 import 'slick-carousel';
+import 'magnific-popup';
+
 $(document).ready(function() {
 
-
+  
   function setSlider(url, slides) {
     var imgArr;
     $.ajax({
@@ -33,8 +18,8 @@ $(document).ready(function() {
       if (i <= slides) {
         var imgBig = '<img data-lazy="' + e.url + '" alt="' + e.title + '">';
         var imgThumb = '<img src="' + e.thumbnailUrl + '" alt="' + e.title + '">';
-        $('.slider-big1').append(imgBig);
-        $('.slider-thumbs1').append(imgThumb);
+        $('.js-slider-big1').append(imgBig);
+        $('.js-slider-thumbs1').append(imgThumb);
       } else {
         currentIndex = i + 1;
         return false;
@@ -44,32 +29,34 @@ $(document).ready(function() {
 
 
 
-    $('.slider-big1').slick({
+    $('.js-slider-big1').slick({
       slidesToShow: 1,
       slidesToScroll: 1,
       fade: true,
-      asNavFor: '.slider-thumbs1',
+      asNavFor: '.js-slider-thumbs1',
       lazyLoad: 'ondemand',
       infinite: false,
     });
-    $('.slider-thumbs1').slick({
+    $('.js-slider-thumbs1').slick({
       slidesToShow: slides,
       slidesToScroll: 1,
-      asNavFor: '.slider-big1',
+      asNavFor: '.js-slider-big1',
       focusOnSelect: true,
       infinite: false,
       arrows: false,
     });
 
+    // Popup
 
-    $('.slider-thumbs1').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+
+    $('.js-slider-thumbs1').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
       if (slick.$slides.length - currentSlide <= slides + 1) {
         $.each(imgArr, function(i, e) {
           if (i >= currentIndex && i <= currentIndex + slides * 2) {
             var imgBig = '<div><div><img style="width: 100%; display: inline-block;" data-lazy="' + e.url + '" alt="' + e.title + '"></div></div>';
             var imgThumb = '<div><div><img style="width: 100%; display: inline-block;" src="' + e.thumbnailUrl + '" alt="' + e.title + '"></div></div>';
-            $('.slider-big1').slick('slickAdd', imgBig);
-            $('.slider-thumbs1').slick('slickAdd', imgThumb);
+            $('.js-slider-big1').slick('slickAdd', imgBig);
+            $('.js-slider-thumbs1').slick('slickAdd', imgThumb);
           } else if (i === currentIndex + slides) {
             currentIndex = i + 1;
             return false;
